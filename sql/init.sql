bc656203ea751e4223f6e16f144859bfad748e53
ddonano <3127408+ddonano@users.noreply.github.com>
2023-05-04 17:58:41 +0800

    Update init.sql
    配置变更

--- sql/init.sql
+++ sql/init.sql
@@ -25,7 +25,7 @@ CREATE TABLE `t_space_config` (
   `sort` int(11) DEFAULT '0' COMMENT '排列顺序',
   PRIMARY KEY (`id`),
   KEY `IDX_SPACE_CONFIG_CODE` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 /*
 删除excel初始化方式，额外NPC创建表，从该表读取配置，创建NPC，方便跟管理后台联通，kbengine 通过interface 定时任务创建和删除NPC
@@ -53,9 +53,13 @@ CREATE TABLE `t_entity_config` (
   `space_type` int(11) DEFAULT '0' COMMENT '对应t_space_config表里的ID',
   `remark` varchar(512) COLLATE utf8mb4_bin DEFAULT NULL COMMENT '描述',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-INSERT INTO `t_space_config` (`code`, `type`, `name`, `location`, `tip`, `desc`, `pic`, `url`, `status`, `del_flag`, `create_by`, `update_by`, `create_time`, `update_time`, `module_id`, `spawn_pos`, `create_status`, `space_id`, `partition_size`, `sort`) VALUES ('kbengine_unity3d_demo', '1', '中央广场测试', '0,0,0', NULL, NULL, '/picture/2023/03/22/1d0e48fd-9e57-4925-b4e7-0fc5bed262fa.png', NULL, '1', '2', NULL, 'admin', NULL, '2023-03-22 20:09:07', '0', '0,1,0', '1', '14', '100', '2');
+INSERT INTO `t_space_config` (`id`,`code`, `type`, `name`, `location`, `tip`, `desc`, `pic`, `url`, `status`, `del_flag`, `create_by`, `update_by`, `create_time`, `update_time`, `module_id`, `spawn_pos`, `create_status`, `space_id`, `partition_size`, `sort`) VALUES ('1','kbengine_unity3d_demo', '1', '中央广场测试', '0,0,0', NULL, NULL, '/picture/2023/03/22/1d0e48fd-9e57-4925-b4e7-0fc5bed262fa.png', NULL, '1', '2', NULL, 'admin', NULL, '2023-03-22 20:09:07', '0', '0,1,0', '1', '14', '100', '2');
+
+INSERT INTO `t_entity_config` (`name`, `run_speed`, `move_speed`, `entity_type`, `dialog_id`, `model_id`, `etype`, `position`, `direction`, `status`, `entity_status`, `create_time`, `update_time`, `create_by`, `update_by`, `move_type`, `del_flag`, `dialog_message`, `space_type`, `remark`) VALUES ('测试NPC', '0', '0', 'NPC', '0', 'a1106694216173498368', '2', '0,-0.7,3', '60', '0', '1', '2023-04-18 13:39:56', '2023-04-18 13:39:56', 'admin', 'admin', '0', '0', '链接地址1|https://cn.bing.com/;lianjiedizhi2|https://www.baidu.com/', '1', 'NPCA');
+INSERT INTO `t_entity_config` (`name`, `run_speed`, `move_speed`, `entity_type`, `dialog_id`, `model_id`, `etype`, `position`, `direction`, `status`, `entity_status`, `create_time`, `update_time`, `create_by`, `update_by`, `move_type`, `del_flag`, `dialog_message`, `space_type`, `remark`) VALUES ('62626', '0', '0', 'NPC', '0', 'a1094535645960871936', '1', '0,-0.7,6', '66', '0', '1', '2023-04-18 13:52:45', '2023-04-18 13:53:09', 'admin', 'admin', '0', '0', '阿萨撒所', '1', '6666');
+INSERT INTO `t_entity_config` (`name`, `run_speed`, `move_speed`, `entity_type`, `dialog_id`, `model_id`, `etype`, `position`, `direction`, `status`, `entity_status`, `create_time`, `update_time`, `create_by`, `update_by`, `move_type`, `del_flag`, `dialog_message`, `space_type`, `remark`) VALUES ('NPC啊', '0', '0', 'NPC', '0', 'a1106692226290823168', '2', '0,-0.7,8', '16', '0', '1', '2023-04-25 16:00:52', '2023-04-25 16:19:06', 'admin', 'admin', '0', '0', '66666666666666666', '1', '66666666666666666');
 
 
 
